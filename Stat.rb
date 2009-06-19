54104703ec429f49873647485c3e1b89e9455660
Victor Hsieh <victor@csie.org>
2009-06-20 00:46:44 +0800

    Add pitching and fielding to Stat

--- Stat.rb
+++ Stat.rb
@@ -1,5 +1,5 @@
 class BaseStat
-    def initialize(stats)
+    def initialize(stats={})
         set_stats stats
     end
 
@@ -9,7 +9,7 @@ class BaseStat
         }
     end
 
-    def self.def_stat(abbr, stat, &formula)
+    def self.def_stat(abbr, stat, *aliases, &formula)
         @abbr_map[abbr] = stat
         if block_given?
             define_method stat, formula
@@ -17,6 +17,7 @@ class BaseStat
             attr_reader stat
         end
         alias_method abbr, stat
+        aliases.each {|name| alias_method name, stat}
     end
 
     def self.to_canonical(name)
@@ -30,34 +31,39 @@ class BaseStat
             eval "@#{key} = #{value}.to_f"
         end
     end
+
+
+    def do
+        yield self
+    end
 end
 
 class BattingStat < BaseStat
     def_stat :G, :game
-    def_stat :TPA, :totalPlateApperance
+    def_stat :PA, :plateAppearance, :TPA, :totalPlateAppearance
     def_stat :AB, :atBase
     def_stat :H, :hit # do single + double + triple + homeRun end
 # may need to replace attr_reader with our own return-attr-if-exist-prior-to-function getter
     def_stat :H2B, :double
     def_stat :H3B, :triple
     def_stat :HR, :homeRun
-    def_stat :GS, :grandSlamHomeRun
+    def_stat :GS, :grandSlam
     def_stat :RBI, :runsBattedIn
     def_stat :SAC, :sacrificeBunt
     def_stat :SF, :sacrificeFly
     def_stat :LOB, :leftOnBase
 
     def_stat :SO, :strikeOut
-    def_stat :BB, :basesOnBall
+    def_stat :BB, :baseOnBall
     def_stat :HBP, :hitByPitch
-    def_stat :IBB, :intentionalWalks
+    def_stat :IBB, :intentionalWalk
     def_stat :GO, :groundOut
     def_stat :AO, :flyOut
     def_stat :GIDP, :groundIntoDoublePlay
     def_stat :TP, :triplePlay
 
     def_stat :NP, :numberOfPitch
-    def_stat :R, :runsScored
+    def_stat :R, :runScored
     def_stat :SB, :stolenBase
     def_stat :CS, :caughtStealing
 
@@ -65,16 +71,110 @@ class BattingStat < BaseStat
 
     def_stat :H1B, :single do hit - double - triple - homeRun end
     def_stat :TB, :totalBase do single + 2 * double + 3 * triple + 4 * homeRun end
+    def_stat :XBH, :extraBaseHit do double + triple + homeRun end
 
 
     def_stat :AVG, :batAverage do hit / atBase end
     def_stat :OBP, :onBasePercentage do
         # XXX intentionalWalks doesn't counts in MLB
-        (hit + basesOnBall + hitByPitch + intentionalWalks) /
-        (atBase + basesOnBall + hitByPitch + sacrificeFly + intentionalWalks)
+        (hit + baseOnBall + hitByPitch + intentionalWalk) /
+        (atBase + baseOnBall + hitByPitch + sacrificeFly + intentionalWalk)
     end
     def_stat :SLG, :sluggingPercentage do totalBase / atBase end
     def_stat :OPS, :onBasePercentagePlusSluggingPercentage do
         onBasePercentage + sluggingPercentage
+    end
+end
+
+class PitchingStat < BaseStat
+    def_stat :AO, :flyOut
+    def_stat :APP, :appearance
+    def_stat :BB, :baseOnBall
+    def_stat :BK, :balk
+    def_stat :BS, :blownSave
+    def_stat :CG, :completeGame
+    def_stat :CGL, :completeGameLose
+    def_stat :CS, :caughtStealing
+    def_stat :ER, :earnedRun
+    def_stat :ERA, :earnedRunAverage
+    def_stat :G, :gamesPlayed
+    def_stat :GF, :gamesFinished
+    def_stat :GIDP, :groundIntoDoublePlay
+    def_stat :GO, :groundOut
+    def_stat :GS, :gameStarted
+    def_stat :GSH, :grandSlam
+    def_stat :H, :hit
+    def_stat :HB, :hitBatsmen
+    def_stat :HLD, :hold
+    def_stat :HR, :homeRun
+    def_stat :IBB, :intentionalWalk
+    def_stat :IP, :inningPitched
+    def_stat :IRA, :inheritedRunAllowed
+    def_stat :L, :loss
+    def_stat :LOB, :leftOnBase
+    def_stat :NP, :numberOfPitchThrown
+    def_stat :PA, :plateAppearance
+    def_stat :PK, :pickoff
+    def_stat :R, :run
+    def_stat :RW, :reliefWin
+    def_stat :SB, :stolenBase
+    def_stat :SHO, :shutout
+    def_stat :SO, :strikeOut
+    def_stat :SV, :save
+    def_stat :SVO, :saveOpportunity
+    def_stat :TB, :totalBase
+    def_stat :TP, :triplePlay
+    def_stat :UR, :unearnedRun
+    def_stat :W, :win
+    def_stat :WP, :wildPitch
+    def_stat :XBA, :extraBaseHitAllowed
+
+
+    def_stat :AVG, :opponentsBattingAverage do hit / battersFaced end # not precise
+    def_stat :BB9, :walksPerNineInnings do baserunnersPer9Innings / inningPitched * 9 end
+    def_stat :BF, :battersFaced do inningPitched * 3 + hit + baseOnBall + hitBatsmen end # + number of batters that reached base on error against the pitcher
+    def_stat :GO_AO, :groundOutFlyOutRatio do groundOut / flyOut end
+    def_stat :H9, :hitPerNineInnings do hit / inningPitched * 9 end
+#    def_stat :I_GS, :inningPerGamesStarted do inningPitched(as starter) / gameStarted end
+    def_stat :K_9, :strikeOutsPerNineInnings do strikeOut / inningPitched * 9 end
+    def_stat :K_BB, :strikeOutOverWalkRatio do strikeOut / baseOnBall end
+    def_stat :LIPS, :lateInningPressureSituations # The batting average allowed by the pitcher to opposing hitters in Late Inning Pressure Situations, which is any at-bat in the seventh inning or later, with the batting team either leading by one run, tied, or has the potential tying run on base, at bat, or on deck.
+    def_stat :MB_9, :baserunnersPer9Innings do (hit + baseOnBall) / inningPitched * 9 end
+    def_stat :OBA, :onBaseAgainst do (hit + baseOnBall + hitBatsmen) / battersFaced end # not precisely
+#    def_stat :P_GS, :pitchesPerStart
+    def_stat :P_IP, :pitchesPerInningsPitched do numberOfPitchThrown / inningPitched end
+    def_stat :SLG, :sluggingPercentage do totalBase / battersFaced end # not precise
+    def_stat :WHIP, :walksPlusHitsOverInningsPitched do (hit + baseOnBall) / inningPitched end 
+    def_stat :WPCT, :winningPercentage do win / (win + loss) end
+end
+
+class FieldingStat < BaseStat
+    def_stat :A, :assist
+    def_stat :CS, :caughtStealing
+    def_stat :DP, :doublePlayer
+    def_stat :E, :error
+    def_stat :G, :gamesPlayed
+    def_stat :INN, :inningPlayed
+    def_stat :OFA, :outfieldAssist
+    def_stat :PB, :passedBall
+    def_stat :PO, :putout
+    def_stat :SB, :stolenBase
+    def_stat :TC, :totalChance
+    def_stat :TP, :triplePlay
+
+    def_stat :FPCT, :fieldingPercentage do (putout + assist) / (putout + assist + error) end
+
+    # FIXME it uses batting stat!
+    def_stat :DEF, :defensiveEfficiencyRating do 1 - (hit - homeRun) / (plateAppearance - homeRun - baseOnBall - hitByPitch - strikeOut) end
+
+    def_stat :RF, :rangeFactor do (putout + assist) * 9 / inning end
+end
+
+class BaseballStat # < BaseStat
+    attr_accessor :batting, :pitching, :fielding
+
+    # TODO switch hitter, utility man stat
+    def initialize()
+        @batting, @pitching, @fielding = BattingStat.new, PitchingStat.new, FieldingStat.new
     end
 end
--- Stat_test.rb
+++ Stat_test.rb
@@ -3,14 +3,32 @@
 require 'test/unit'
 require 'Stat'
 
+class MockContext
+    def initialize(player)
+        @player = player
+    end
+
+    def each_players(&b)
+        @player.do {|x| yield x}
+    end
+end
+
 class TestStat < Test::Unit::TestCase
     def setup
-        @batting = BattingStat.new(
+        @player = BaseballStat.new
+        @player.batting.set_stats(
             :G => 66, :PA => 279, :AB => 240, :H => 88, :H2B=> 17, :H3B => 2,
             :HR => 16, :RBI => 39, :SAC => 0, :SF => 6, :SO => 53, :BB => 28,
             :HBP => 2, :IBB => 3, :GIDP => 4, :R => 52,
             :SB => 4, :CS => 1
-        )
+            )
+        @player.pitching.set_stats(
+            :W => 16, :L => 11, :H => 138, :BB => 61, :IP => 150, :HR => 3
+            )
+        @player.fielding.set_stats(
+            :PO => 256, :A => 473, :E => 20
+            )
+        @batting, @pitching, @fielding = @player.batting, @player.pitching, @player.fielding
     end
 
     def test_batting
@@ -20,10 +38,34 @@ class TestStat < Test::Unit::TestCase
         assert_equal(4, @batting.SB)
         assert_equal(53, @batting.H1B)
         assert_equal(157, @batting.TB)
+        assert_equal(279, @batting.TPA)
+        assert_equal(279, @batting.PA)
 
         assert_in_delta(0.367, @batting.AVG, 0.0005)
         assert_in_delta(0.434, @batting.OBP, 0.0005)
         assert_in_delta(0.654, @batting.SLG, 0.0005)
         assert_in_delta(1.088, @batting.OPS, 0.0005)
+    end
+
+    def test_pitching
+        assert_equal(16, @pitching.W)
+        assert_equal(3, @pitching.HR)
+        assert_equal(138, @pitching.H)
+        assert_in_delta(1.33, @pitching.WHIP, 0.005)
+        assert_in_delta(0.593, @pitching.WPCT, 0.0005)
+    end
+
+    def test_fielding
+        assert_equal(256, @fielding.PO)
+        assert_equal(256, @fielding.putout)
+        assert_in_delta(0.973, @fielding.FPCT, 0.0005)
+    end
+
+    def test_delegate
+        league = MockContext.new(@batting)
+        league.each_players {|p|
+            puts self
+            puts p
+        }
     end
 end
