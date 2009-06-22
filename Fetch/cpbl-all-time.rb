b8a8eb64807406d090b10816cf212dd55770ceaf
Victor Hsieh <victor@csie.org>
2009-06-22 16:39:20 +0800

    Stat extractor of personal stats

--- Fetch/cpbl-all-time.rb
+++ Fetch/cpbl-all-time.rb
@@ -1,7 +1,51 @@
-#!/usr/bin/ruby -Ku
+#!/usr/bin/ruby
 
 def collect_players_from_team_in_year(page)
     open(page, 'r') do |f|
         f.to_enum.grep(/Pno=/).map {|line| line.match(/Pno=(\w+)/); $1}
     end
+end
+
+def _read_until(f, pattern)
+    until f.readline.match(pattern); end
+    $1
+end
+
+def smart_type_casting(value)
+    value.match(/^\d+$/) ? value.to_i : value
+end
+
+def _read_personal_stat_by_row(f)
+    results = []
+    while f.readline.match(/<tr class="Report_.*Item"/)
+        stat = []
+        while f.readline.match(/<td>(.*)<\/td>/)
+            stat.push smart_type_casting $1
+        end
+        results.push stat
+    end
+    results.pop # drop the "total" line
+    results
+end
+
+def collect_player_stat(page)
+    stats = {}
+    open(page, 'r') do |f|
+    begin
+        until f.eof?
+            type = _read_until(f, /images\/hd\/player(\d)\.gif/)
+            f.readline
+            _read_until f, /<\/tr>/
+            case type.to_i
+            when 1; kind = :pitching
+            when 2; kind = :batting
+            when 3; kind = :fielding
+            else; next
+            end
+            stats[kind] = _read_personal_stat_by_row f
+        end
+    rescue EOFError
+    end
+    end
+    stats
 end
--- Fetch/cpbl-all-time_test.rb
+++ Fetch/cpbl-all-time_test.rb
@@ -1,11 +1,34 @@
-#!/usr/bin/ruby
+#!/usr/bin/ruby -Ku
 
 require 'test/unit'
 require 'cpbl-all-time'
+#require '../Stat'
 
 class TestStat < Test::Unit::TestCase
     def test_collect_players
         assert_equal(['C016', 'C011', 'C023', 'C077', 'C048', 'C039', 'C007', 'C017', 'C001', 'C009', 'C012'].sort, collect_players_from_team_in_year('test/93-eagle-pitcher.html').sort)
         assert_equal(['E002', 'E033', 'B009', 'E026', 'E044', 'E019', 'E022', 'E001', 'E035', 'E032', 'E043', 'E024', 'E025', 'E029', 'E009', 'E020', 'E021'].sort, collect_players_from_team_in_year('test/90-elephant-hitter.html').sort)
+    end
+
+    def test_collect_hitter_stat
+        stats = collect_player_stat('test/hitter_career.html')
+        assert_equal(4, stats[:batting][0][0])
+        assert_equal(90, stats[:batting][0][2])
+        assert_equal(5, stats[:batting].size)
+
+        assert_equal(195, stats[:fielding][0][3])
+        assert_equal(94, stats[:fielding][4][4])
+        assert_equal(5, stats[:fielding].size)
+    end
+
+    def test_collect_pitcher_stat
+        stats = collect_player_stat('test/pitcher_career.html')
+        assert_equal(26, stats[:pitching][0][2])
+        assert_equal(12, stats[:pitching][5][7])
+        assert_equal(7, stats[:pitching].size)
+
+        assert_equal(42, stats[:fielding][0][3])
+        assert_equal(9, stats[:fielding][4][4])
+        assert_equal(7, stats[:fielding].size)
     end
 end
