fccbd8200b1569bd2fc73c8ce5755272dff17ae7
Victor Hsieh <victor@csie.org>
2009-07-04 17:58:22 +0800

    Extract class and rename methods

--- Fetch/fetch-cpbl-all-time.rb
+++ Fetch/fetch-cpbl-all-time.rb
@@ -6,173 +6,179 @@ require '../Stat'
 require '../BaseballUtils'
 
 
-$TeamName = {
-    :C01 => '時報', :A01 => '第一', :B01 => '俊國', :B02 => '興農',
-    :G01 => '誠泰', :G02 => '米迪亞', :E01 => '兄弟', :W01 => '中信',
-    :L01 => '統一', :A02 => 'La New', :D01 => '味全', :T01 => '三商'
-}
-$TeamCode = $TeamName.invert
+class CPBLProxy
 
-def fetch_team_players(team, year, kind)
-    BaseballUtils::cached("data/raw/players/#{team}-{#{year}-#{kind}.data") {
-        CPBLStatExtractor::collect_players_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&qyear=#{year}")
+    $TeamName = {
+        :C01 => '時報', :A01 => '第一', :B01 => '俊國', :B02 => '興農',
+        :G01 => '誠泰', :G02 => '米迪亞', :E01 => '兄弟', :W01 => '中信',
+        :L01 => '統一', :A02 => 'La New', :D01 => '味全', :T01 => '三商'
     }
-end
+    $TeamCode = $TeamName.invert
 
-def all_players
-    BaseballUtils::cached('data/players.data') {
-        all_players = []
-        $TeamName.each_key {|team|
-            1990.upto(2009) {|year|
-                hitters = fetch_team_players(team, year, 'Hitter')
-                all_players.push *hitters unless hitters.empty?
-
-                pitchers = fetch_team_players(team, year, 'Pitcher')
-                all_players.push *pitchers unless hitters.empty?
-            }
+    def team_players(team, year, kind)
+        BaseballUtils::cached("data/raw/players/#{team}-{#{year}-#{kind}.data") {
+            CPBLStatExtractor::collect_players_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&qyear=#{year}")
         }
-        all_players.sort.uniq
-    }
-end
+    end
 
-def fetch_player(id)
-    BaseballUtils::cached("data/raw/personal/#{id}") {
-        stat = CPBLStatExtractor::collect_player_career_stat("http://www.cpbl.com.tw/personal_Rec/pbat_personal.aspx?Pno=#{id}")
-        stat2 = CPBLStatExtractor::collect_player_career_stat("http://www.cpbl.com.tw/personal_Rec/pbat_personal.aspx?Pno=#{id}&pbatpage=2")
-        if stat2.has_key? :pitching
-            stat[:pitching2] = stat2[:pitching]
-        elsif stat2.has_key? :batting
-            stat[:batting2] = stat2[:batting]
-        else
-            p "how come!? (#{id})", stat2
-        end
-        sleep 0.5
-        stat
-    }
-end
+    def all_players
+        BaseballUtils::cached('data/players.data') {
+            all_players = []
+            $TeamName.each_key {|team|
+                1990.upto(2009) {|year|
+                    hitters = fetch_team_players(team, year, 'Hitter')
+                    all_players.push *hitters unless hitters.empty?
 
-def fetch_fielding_position_detail(id)
-    BaseballUtils::cached("data/raw/fielding/#{id}") {
-        results = []
-        stat = fetch_player(id)
-        unless stat[:fielding]
-            p "Player #{id} has no fielding stat"
-            return [] 
-        end
-
-        stat[:fielding].map {|stat| stat[0] + 1989}.each {|year|
-            stat = CPBLStatExtractor::collect_fielding_detail("http://www.cpbl.com.tw/personal_Rec/pdf_detail.aspx?pbyear=#{year}&Pno=#{id}")
-            results.push *stat unless stat.empty?
-        }
-        sleep 0.5
-        results
-    }
-end
-
-def fetch_pitcher_stat(id)
-    BaseballUtils::cached("data/raw/pitcher/#{id}") {
-        stat = fetch_player(id)
-        if stat.has_key?(:pitching)
-            result = {:page1 => [], :page2 => []}
-            stat[:pitching].map {|s| s[0].to_s.rjust(2, '0')}.each {|year|
-                puts "http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}"
-                stats = CPBLStatExtractor::collect_pitcher_by_game_in_year("http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}")
-                stats2 = CPBLStatExtractor::collect_pitcher_by_game_in_year("http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}&pbatpage=2")
-                result[:page1].push stats
-                result[:page2].push stats2
+                    pitchers = fetch_team_players(team, year, 'Pitcher')
+                    all_players.push *pitchers unless hitters.empty?
+                }
             }
+            all_players.sort.uniq
+        }
+    end
+
+    def pitcher_stat(id)
+        translate_stats_pair(
+                fetch_pitcher_stat(id),
+                'Name,G,PA,AB,RBI,R,H,H1B,H2B,H3B,HR,TB,DP', \
+                'Name,SH,SF,BB,IBB,TOTAL_BB,SO,SB,CS') {|s|
+            s[:HBP] = s[:TOTAL_BB] - s[:BB] - s[:IBB]
+            s.delete(:TOTAL_BB)
+            s.delete(:H1B)
+        }
+    end
+
+    def personal_fielding_detail(id)
+        translate_stats(
+                fetch_fielding_position_detail(id),
+                'Year,Pos,G,TC,PO,A,E,DP,TP,PB,CS,SB') {|s|
+            s[:Year] += 1989
+        }
+    end
+
+    def personal_fielding(id)
+        translate_stats(fetch_player(id)[:fielding],
+                'Year,Team,G,TC,PO,A,E,DP,TP,PB,CS,SB') {|s| s[:Year] += 1989 }
+    end
+
+    def batter_stats_by_team_year(team, year)
+        translate_stats_pair(
+            fetch_player_stats_by_team_year("team_batting", "Hitter", team, year),
+                'Name,G,PA,AB,RBI,R,H,H1B,H2B,H3B,HR,TB,DP',
+                'Name,SH,SF,BB,IBB,TOTAL_BB,SO,SB,CS', \
+                {:Year => year, :Team => team}) {|s|
+            s[:HBP] = s[:TOTAL_BB] - s[:BB] - s[:IBB]
+            s.delete(:TOTAL_BB)
+            s.delete(:H1B)
+        }
+    end
+
+    def pitcher_stats_by_team_year(team, year)
+        translate_stats_pair(
+            fetch_player_stats_by_team_year("team_pitching", "Pitcher", team, year),
+                'Name,G,GS,HLDO,SVO,W,L,TIE,SV,SVO_SV,HLD,,CG,SHO,NOBB',
+                'Name,IP,PA,NP,H,HR,SAC,SF,BB,IBB,HBP,SO,WP,BK,R,ER', \
+                {:Year => year, :Team => team}) {|s|
+            s[:SVO] = s[:SV] + s[:SVO_SV]
+            s[:IP] = BaseballUtils::fix_inning_rounding(s[:IP])
+            s.delete(:SVO_SV)
+        }
+    end
+
+
+    private
+
+    def fetch_player(id)
+        BaseballUtils::cached("data/raw/personal/#{id}") {
+            stat = CPBLStatExtractor::collect_player_career_stat("http://www.cpbl.com.tw/personal_Rec/pbat_personal.aspx?Pno=#{id}")
+            stat2 = CPBLStatExtractor::collect_player_career_stat("http://www.cpbl.com.tw/personal_Rec/pbat_personal.aspx?Pno=#{id}&pbatpage=2")
+            if stat2.has_key? :pitching
+                stat[:pitching2] = stat2[:pitching]
+            elsif stat2.has_key? :batting
+                stat[:batting2] = stat2[:batting]
+            else
+                p "how come!? (#{id})", stat2
+            end
+            sleep 0.5
+            stat
+        }
+    end
+
+    def fetch_fielding_position_detail(id)
+        BaseballUtils::cached("data/raw/fielding/#{id}") {
+            results = []
+            stat = fetch_player(id)
+            unless stat[:fielding]
+                p "Player #{id} has no fielding stat"
+                return [] 
+            end
+
+            stat[:fielding].map {|stat| stat[0] + 1989}.each {|year|
+                stat = CPBLStatExtractor::collect_fielding_detail("http://www.cpbl.com.tw/personal_Rec/pdf_detail.aspx?pbyear=#{year}&Pno=#{id}")
+                results.push *stat unless stat.empty?
+            }
+            sleep 0.5
+            results
+        }
+    end
+
+    def fetch_pitcher_stat(id)
+        BaseballUtils::cached("data/raw/pitcher/#{id}") {
+            stat = fetch_player(id)
+            if stat.has_key?(:pitching)
+                result = {:page1 => [], :page2 => []}
+                stat[:pitching].map {|s| s[0].to_s.rjust(2, '0')}.each {|year|
+                    puts "http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}"
+                    stats = CPBLStatExtractor::collect_pitcher_by_game_in_year("http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}")
+                    stats2 = CPBLStatExtractor::collect_pitcher_by_game_in_year("http://www.cpbl.com.tw/personal_Rec/ppitch_single.aspx?Gno=01&pbyear=#{year}&Pno=#{id}&pbatpage=2")
+                    result[:page1].push stats
+                    result[:page2].push stats2
+                }
+                sleep 0.3
+                result
+            else
+                nil
+            end
+        }
+    end
+
+    def fetch_player_stats_by_team_year(dir, kind, team, year)
+        BaseballUtils::cached("data/raw/#{dir}/#{team}-#{year}") {
+            stats = {}
+            stats[:page1] = CPBLStatExtractor::collect_players_stats_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&page=1&qyear=#{year}")
+            stats[:page2] = CPBLStatExtractor::collect_players_stats_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&page=2&qyear=#{year}")
             sleep 0.3
-            result
-        else
-            nil
-        end
-    }
-end
+            stats
+        }
+    end
 
-def get_pitcher_stat(id)
-    translate_stats_pair(
-            fetch_pitcher_stat(id),
-            'Name,G,PA,AB,RBI,R,H,H1B,H2B,H3B,HR,TB,DP', \
-            'Name,SH,SF,BB,IBB,TOTAL_BB,SO,SB,CS') {|s|
-        s[:HBP] = s[:TOTAL_BB] - s[:BB] - s[:IBB]
-        s.delete(:TOTAL_BB)
-        s.delete(:H1B)
-    }
-end
+    def csv_to_syms(csv)
+        csv.split(',').map {|x| x.empty? ? nil : x.to_sym}
+    end
 
-def get_personal_fielding_detail(id)
-    translate_stats(
-            fetch_fielding_position_detail(id),
-            'Year,Pos,G,TC,PO,A,E,DP,TP,PB,CS,SB') {|s|
-        s[:Year] += 1989
-    }
-end
+    def merge_csv_and_stats(csv, stats)
+        stats.map {|stat| Hash[csv_to_syms(csv).zip(stat)]}
+    end
 
-def get_personal_fielding(id)
-    translate_stats(fetch_player(id)[:fielding],
-            'Year,Team,G,TC,PO,A,E,DP,TP,PB,CS,SB') {|s| s[:Year] += 1989 }
-end
+    def fix_stat(stat)
+        stat[:Name].gsub!(/ /, '') if stat.has_key? :Name
+        stat.delete(nil)
+        stat
+    end
 
-def fetch_player_stats_by_team_year(dir, kind, team, year)
-    BaseballUtils::cached("data/raw/#{dir}/#{team}-#{year}") {
-        stats = {}
-        stats[:page1] = CPBLStatExtractor::collect_players_stats_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&page=1&qyear=#{year}")
-        stats[:page2] = CPBLStatExtractor::collect_players_stats_from_team_in_year("http://www.cpbl.com.tw/teams/Team_#{kind}.aspx?Tno=#{team}&page=2&qyear=#{year}")
-        sleep 0.3
-        stats
-    }
-end
+    def translate_stats(stats, header, additional={})
+        merge_csv_and_stats(header, stats).map {|stat|
+            s = fix_stat(stat.merge additional)
+            yield s if block_given?
+            s
+        }
+    end
 
-def csv_to_syms(csv)
-    csv.split(',').map {|x| x.empty? ? nil : x.to_sym}
-end
-
-def merge_csv_and_stats(csv, stats)
-    stats.map {|stat| Hash[csv_to_syms(csv).zip(stat)]}
-end
-
-def fix_stat(stat)
-    stat[:Name].gsub!(/ /, '') if stat.has_key? :Name
-    stat.delete(nil)
-    stat
-end
-
-def translate_stats(stats, header, additional={})
-    merge_csv_and_stats(header, stats).map {|stat|
-        s = fix_stat(stat.merge additional)
-        yield s if block_given?
-        s
-    }
-end
-
-def translate_stats_pair(stats, header1, header2, additional={})
-    merge_csv_and_stats(header1, stats[:page1]).zip(
-            merge_csv_and_stats(header2, stats[:page2])).map {|s1,s2|
-        yield s = fix_stat(s1.merge s2.merge additional)
-        s
-    }
-end
-
-def get_batter_stats_by_team_year(team, year)
-    translate_stats_pair(
-        fetch_player_stats_by_team_year("team_batting", "Hitter", team, year),
-            'Name,G,PA,AB,RBI,R,H,H1B,H2B,H3B,HR,TB,DP',
-            'Name,SH,SF,BB,IBB,TOTAL_BB,SO,SB,CS', \
-            {:Year => year, :Team => team}) {|s|
-        s[:HBP] = s[:TOTAL_BB] - s[:BB] - s[:IBB]
-        s.delete(:TOTAL_BB)
-        s.delete(:H1B)
-    }
-end
-
-def get_pitcher_stats_by_team_year(team, year)
-    translate_stats_pair(
-        fetch_player_stats_by_team_year("team_pitching", "Pitcher", team, year),
-            'Name,G,GS,HLDO,SVO,W,L,TIE,SV,SVO_SV,HLD,,CG,SHO,NOBB',
-            'Name,IP,PA,NP,H,HR,SAC,SF,BB,IBB,HBP,SO,WP,BK,R,ER', \
-            {:Year => year, :Team => team}) {|s|
-        s[:SVO] = s[:SV] + s[:SVO_SV]
-        s[:IP] = BaseballUtils::fix_inning_rounding(s[:IP])
-        s.delete(:SVO_SV)
-    }
+    def translate_stats_pair(stats, header1, header2, additional={})
+        merge_csv_and_stats(header1, stats[:page1]).zip(
+                merge_csv_and_stats(header2, stats[:page2])).map {|s1,s2|
+            yield s = fix_stat(s1.merge s2.merge additional)
+            s
+        }
+    end
 end
--- Fetch/fetch-cpbl-all-time_test.rb
+++ Fetch/fetch-cpbl-all-time_test.rb
@@ -4,13 +4,17 @@ require 'test/unit'
 require 'fetch-cpbl-all-time'
 
 class TestStat < Test::Unit::TestCase
-    def test_fetch_team_players
-        players = fetch_team_players(:C01, 1995, 'Hitter')
+    def setup
+        @cpbl = CPBLProxy.new
+    end
+
+    def test_team_players
+        players = @cpbl.team_players(:C01, 1995, 'Hitter')
         assert_equal(20, players.size)
     end
 
-    def test_fetch_batter_stats_by_team_year
-        stats = get_batter_stats_by_team_year('E01', 1995)
+    def test_batter_stats_by_team_year
+        stats = @cpbl.batter_stats_by_team_year('E01', 1995)
         assert_equal(18, stats.size)
         assert_equal(1995, stats[0][:Year])
         assert_equal(97, stats[0][:G])
@@ -19,8 +23,8 @@ class TestStat < Test::Unit::TestCase
         assert_equal(9, stats[0][:CS])
     end
 
-    def test_fetch_pitcher_stats_by_team_year
-        stats = get_pitcher_stats_by_team_year('E01', 1995)
+    def test_pitcher_stats_by_team_year
+        stats = @cpbl.pitcher_stats_by_team_year('E01', 1995)
         assert_equal(13, stats.size)
         assert_equal("陳義信", stats[0][:Name])
         assert_equal(26, stats[0][:G])
@@ -35,15 +39,15 @@ class TestStat < Test::Unit::TestCase
         assert_equal(20, stats[0][:HR])
     end
 
-    def test_fetch_personal_fielding_detail_of_multi_position
-        stats = get_personal_fielding_detail('A013').select {|s| s[:Year] == 2007}
+    def test_personal_fielding_detail_of_multi_position
+        stats = @cpbl.personal_fielding_detail('A013').select {|s| s[:Year] == 2007}
         assert_equal(5, stats.size)
         assert_equal(3, stats[1][:G])
         assert_equal(24, stats[4][:PO])
     end
 
-    def test_fetch_personal_fielding_detail_in_year
-        stats = get_personal_fielding('A037')
+    def test_personal_fielding_detail_in_year
+        stats = @cpbl.personal_fielding('A037')
         assert_equal(18, stats[0][:G])
         assert_equal(3, stats[5][:G])
         assert_equal('統一', stats[6][:Team])
